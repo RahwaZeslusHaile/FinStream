0613e01763147941d12bb2ff50efd9a22188da03
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-06-23 19:05:23 +0100

    fix:Run ruff format to ensure standard compliance

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,7 @@ import os
 
 import boto3
 
-TABLE_NAME = os.getenv("TABLE_NAME","finstream-positions")
+TABLE_NAME = os.getenv("TABLE_NAME", "finstream-positions")
 REGION = os.getenv("REGION_NAME", "us-east-1")
-dynamodb = boto3.resource('dynamodb', region_name=REGION)
+dynamodb = boto3.resource("dynamodb", region_name=REGION)
 table = dynamodb.Table(TABLE_NAME)
-
-
